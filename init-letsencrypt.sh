d6d6eaaca05867ea0bea10d0ca0af504e6e12194
gereltod <gereltod.s@gmail.com>
2021-12-30 21:56:01 +0800

    asdf

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=testrewardly.tk www.testrewardly.tk
+domains=*.testrewardly.tk
 rsa_key_size=4096
 data_path="./data/certbot"
 email="gereltod.s@gmail.com" # Adding a valid address is strongly recommended
